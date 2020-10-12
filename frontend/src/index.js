4ba1cf4ed545fc34238fd2d4909e206e9d6fae12
parimal <chandrakarparimal2@gmail.com>
2020-10-12 18:14:54 +0530

    kkkk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import '.index.css';
+import './index.css';
 import App from './App';
-import * as serviceWorker from '.serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
